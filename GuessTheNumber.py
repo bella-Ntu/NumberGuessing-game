75fcab94f037f65f65cfe8187c7720be72efb75d
bella-Ntu <isabellantuha3@gmail.com>
2026-04-19 12:15:34 +0300

    Create GuessTheNumber.py
    code

---
+++ GuessTheNumber.py
@@ -0,0 +1,31 @@
+import random
+
+print('WELCOME TO THE NUMBER GUESSING GAME!')
+
+while True:
+    secret_Number = random.randint(1,100)
+    attempts = 0
+
+    print('''\nI've picked a Random Number between 1 & 100.
+            Try to Guess it!     ''')
+    
+    while True:
+        guess = int(input('Your Guess: '))
+        attempts += 1
+
+        if guess < secret_Number:
+            print('Too low!')
+        elif guess > secret_Number:
+            print('Too high!')
+        else:
+            print(f'🎉🎉 Congragulations! You got it in {attempts} tries!🎉🎉')
+            break
+
+    again = input('\nDo u want to play again? [yes / exit]: ').lower()
+
+    if again == 'exit':
+        print('Thanks for playing. Goodbye👋👋')
+        break
+    elif again != 'yes':
+        print("I'll take that as 'exit'. Goodbye!'")
+    break
